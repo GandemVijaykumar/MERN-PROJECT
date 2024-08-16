76dcbd6f082f7bf1501e7ba44f4bbd3ad5998623
GandemVijaykumar <96295043+GandemVijaykumar@users.noreply.github.com>
2024-08-16 12:21:24 +0530

    create signup api route

---
+++ api/controllers/user.controllers.js
@@ -0,0 +1,6 @@
+const test=(req,res)=>{
+    res.json({
+        message:"test is correct",
+    })
+}
+module.exports=test;
--- api/index.js
+++ api/index.js
@@ -1,7 +1,29 @@
-import express from 'express';
+const userRouter =  require('./routes/user.route')
+const authRouter=require('./routes/auth.route')
+require('dotenv').config();
 
+
+
+const express=require('express')
+const mongoose=require('mongoose');
 const app=express();
+
+
+
+app.use(express.json())
+mongoose.connect(process.env.MONGO).then(()=>
+    {
+        console.log('Connected to MongoDB')
+    }).catch((err)=>{
+        console.log(err)
+    })
 app.listen(3000,()=>
 {
     console.log("server is running at port:3000")
 })
+// app.get('/',(req,res)=>{
+//     res.send("hello world")
+// })
+// instead
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
